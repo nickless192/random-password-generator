afbd42e76961b23525f7e106ad24727ac0a35362
Omar Rodriguez <omar.rguez26@gmail.com>
2022-05-23 11:52:05 -0400

    initial commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,32 @@
 // Assignment code here
+var numArray = "0123456789";
+var specialChar = " !"+'"' + "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+var generatePassword = function() {
+  window.alert("Let's first choose the password criteria:");
+
+    for (var i = 0; i < 10; i++) {
+      window.alert(specialChar.charAt(Math.floor(Math.random()*31)));
+    }
+  
+
+  // var containsLowerCase = window.confirm("Do you want the password to include lowercase letters?");
+
+  // var containsUpperCase = window.confirm("Do you want the password to include upper letters?");
+
+  // var containsNumeric = window.confirm("Do you want the password to include numbers?");
+
+  // var containsSpecialChars = window.confirm("Do you want the password to include special characters?");
+
+  // var passwordLength = 0 ;
+
+  // while (passwordLength < 8 || passwordLength > 128) {
+  //   passwordLength = window.prompt("How long would you like the password to be? Please choose between 8 and 128 characters.");
+  // }
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
