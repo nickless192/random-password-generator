b5a560587ff6165c19ab9c6b64ccf26fc892aad5
Omar Rodriguez <omar.rguez26@gmail.com>
2022-05-24 16:01:06 -0400

    check to ensure at least 1 char type is selected

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-var numArray = "0123456789";
+var numericChar = "0123456789";
 var specialChar = "!"+'"' + "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -13,55 +13,67 @@ var characterSelect = function(charString) {
 
 }
 
+var chooseCriteria = function(characterType) {
+  return window.confirm("Do you want the password to include "+ characterType + " characters?");
+}
+
 var generatePassword = function() {
   window.alert("Let's first choose the password criteria:");
 
-    // for (var i = 0; i < 10; i++) {
-    //   window.alert(specialChar.charAt(Math.floor(Math.random()*31)));
-    // }
-  
+  var containsLowerCase = false;
 
-  var containsLowerCase = window.confirm("Do you want the password to include lowercase letters?");
+  var containsUpperCase = false;
 
-  var containsUpperCase = window.confirm("Do you want the password to include uppercase letters?");
+  var containsNumeric = false;
 
-  var containsNumeric = window.confirm("Do you want the password to include numbers?");
+  var containsSpecialChars = false;
 
-  var containsSpecialChars = window.confirm("Do you want the password to include special characters?");
+  // this loop will ensure are least one character type is selected
+  while (!containsLowerCase && !containsUpperCase && !containsNumeric && !containsSpecialChars) {
 
-  window.alert("Here is a recap of your new password criteria: ");
 
-  if (containsLowerCase) {
-    window.alert("The password will include lowercase letters.");
-  }
-  else {
-    window.alert("The password will NOT contain lowercase letters.");
-  }
-
-  if (containsUpperCase) {
-    window.alert("The password will include uppercase letters.");
-  }
-  else {
-    window.alert("The password will NOT contain uppercase letters.");
-  }
-
-  if (containsNumeric) {
-    window.alert("The password will include numbers.");
-  }
-  else {
-    window.alert("The password will NOT contain numbers.");
-  }
-
-  if (containsSpecialChars) {
-    window.alert("The password will include special characters.");
-  }
-  else {
-    window.alert("The password will NOT contain special characters.");
-  }
-
-  if (!containsLowerCase && !containsUpperCase && !containsNumeric && !containsSpecialChars) {
-    window.alert("Please select at least one character type!");
-
+    var containsLowerCase = chooseCriteria("lowercase");
+    
+    var containsUpperCase = chooseCriteria("uppercase");
+    
+    var containsNumeric = chooseCriteria("numeric");
+    
+    var containsSpecialChars = chooseCriteria("special");
+    
+    window.alert("Here is a recap of your new password criteria: ");
+    
+    if (containsLowerCase) {
+      window.alert("The password will include lowercase characters.");
+    }
+    else {
+      window.alert("The password will NOT contain lowercase characters.");
+    }
+    
+    if (containsUpperCase) {
+      window.alert("The password will include uppercase characters.");
+    }
+    else {
+      window.alert("The password will NOT contain uppercase characters.");
+    }
+    
+    if (containsNumeric) {
+      window.alert("The password will include numerica characters.");
+    }
+    else {
+      window.alert("The password will NOT contain numeri characters.");
+    }
+    
+    if (containsSpecialChars) {
+      window.alert("The password will include special characters.");
+    }
+    else {
+      window.alert("The password will NOT contain special characters.");
+    }
+    
+    if (!containsLowerCase && !containsUpperCase && !containsNumeric && !containsSpecialChars) {
+      window.alert("Please select at least one character type!");
+      
+    }
   }
 
 
@@ -77,7 +89,9 @@ var generatePassword = function() {
   var includedNumbers = false;
   var includedSpecialChar = false;
 
-  for (var i = 0; i < passwordLength; i++) {
+  //debugger;
+  //for (var i = 0; i < passwordLength; i++) {
+  while (passwordStr.length < passwordLength) {
     var charType = Math.floor(Math.random()*4);
 
     if (charType === 0 && containsLowerCase) {
@@ -87,41 +101,39 @@ var generatePassword = function() {
       passwordStr = passwordStr + characterSelect(upperCase);
       includedUpperCase = true;
     } else if (charType === 2 && containsNumeric) {
-      passwordStr = passwordStr + characterSelect(numArray);
+      passwordStr = passwordStr + characterSelect(numericChar);
       includedNumbers = true;
     } else if (charType === 3 && containsSpecialChars) {
       passwordStr = passwordStr + characterSelect(specialChar);
       includedSpecialChar = true;
-    } else {
-      i--;
-    }
+    } 
 
-    switch(charType) {
-      // lowercase
-      case 0: // select random number to choose position of the character
-        // charPosition = Math.floor(Math.random()*lowerCase.length);
-        // passwordStr = passwordStr + lowerCase.charAt(charPosition);
-        passwordStr = passwordStr + characterSelect(lowerCase);
-        break;
-      // uppercase
-      case 1: 
-      // charPosition = Math.floor(Math.random()*upperCase.length);
-      // passwordStr = passwordStr + upperCase.charAt(charPosition);
-        passwordStr = passwordStr + characterSelect(upperCase);
-      break;
-      // numbers
-      case 2:
-        // charPosition = Math.floor(Math.random()*numArray.length);
-        // passwordStr = passwordStr + numArray.charAt(charPosition);
-          passwordStr = passwordStr + characterSelect(numArray);
-        break;
-      // special character
-      case 3:
-        // charPosition = Math.floor(Math.random()*specialChar.length);
-        // passwordStr = passwordStr + specialChar.charAt(charPosition);
-        passwordStr = passwordStr + characterSelect(specialChar);
-        break;
-    }
+    // switch(charType) {
+    //   // lowercase
+    //   case 0: // select random number to choose position of the character
+    //     // charPosition = Math.floor(Math.random()*lowerCase.length);
+    //     // passwordStr = passwordStr + lowerCase.charAt(charPosition);
+    //     passwordStr = passwordStr + characterSelect(lowerCase);
+    //     break;
+    //   // uppercase
+    //   case 1: 
+    //   // charPosition = Math.floor(Math.random()*upperCase.length);
+    //   // passwordStr = passwordStr + upperCase.charAt(charPosition);
+    //     passwordStr = passwordStr + characterSelect(upperCase);
+    //   break;
+    //   // numbers
+    //   case 2:
+    //     // charPosition = Math.floor(Math.random()*numericChar.length);
+    //     // passwordStr = passwordStr + numericChar.charAt(charPosition);
+    //       passwordStr = passwordStr + characterSelect(numericChar);
+    //     break;
+    //   // special character
+    //   case 3:
+    //     // charPosition = Math.floor(Math.random()*specialChar.length);
+    //     // passwordStr = passwordStr + specialChar.charAt(charPosition);
+    //     passwordStr = passwordStr + characterSelect(specialChar);
+    //     break;
+    // }
 
 
   }
